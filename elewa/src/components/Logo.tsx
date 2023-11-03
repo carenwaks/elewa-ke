34db516b301cd3c0cae0df2868c849d5c9c5a79e
Caren Nafula Wakuloba <canafula16@gmail.com>
2023-11-03 13:53:26 +0300

    🎨  feature:update logo image and structue

--- elewa/src/components/Logo.tsx
+++ elewa/src/components/Logo.tsx
@@ -1,8 +1,8 @@
-
+import React from "react";
 function Logo(){
     return (
         <div>
-        <img />
+            <img src="https://res.cloudinary.com/dyl3rncv3/image/upload/v1675690297/elewa-group-website/Icons/SVG/_Logo/Logo_White_arvoyx.svg" alt="" />      
         </div>
     )
 };
