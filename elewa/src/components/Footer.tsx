8e5accadcf74b0bfba1ae371613b44b4c5218adc
Caren Nafula Wakuloba <canafula16@gmail.com>
2023-11-03 16:14:27 +0300

    ✨ feature:create footer component

---
+++ elewa/src/components/Footer.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import FooterItem from "./FooterItem";
+
+function Footer(){
+    return
+    // displaying the footer items
+    (<div>
+        <FooterItem />
+    </div>)
+}
+export default Footer;
