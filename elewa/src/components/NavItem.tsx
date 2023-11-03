91105308cf3d05d2473af25e2f455abfabe1566a
Caren Nafula Wakuloba <canafula16@gmail.com>
2023-11-03 13:20:14 +0300

    🎨 feature:update nav item structure

--- elewa/src/components/NavItem.tsx
+++ elewa/src/components/NavItem.tsx
@@ -1,10 +1,16 @@
 import data from "../json/navItems.json"
 
+// Defining the datatype for the json data
+type NavItemData = {
+    title: string;
+};
+
 function NavItem(){
     return (
         <ul>
-           {data.map((item) => (
-          <div >
+            {/* Mapping the json data and specify the data */}
+           {data.map((item: NavItemData) => (
+          <div key={item.title}>
             <li>{item.title}</li>
           </div>
         ))} 
