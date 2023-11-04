058fdc97976be459b7c6594a71d3bf5da3d071ab
Caren Nafula Wakuloba <canafula16@gmail.com>
2023-11-04 14:35:42 +0300

    ✨ feature:create content 8 component

---
+++ elewa/src/components/Content8.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Content8(){
+    return (
+        <div className="overview-content5">
+            <div><span><p>Become part of our mission. And start Investing</p>.</span></div>
+        </div>
+    )
+}
+export default Content8;
