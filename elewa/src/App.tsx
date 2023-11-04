7135fe50c9dc0db5cd270d50c386cc4c8eda5797
Caren Nafula Wakuloba <canafula16@gmail.com>
2023-11-04 19:12:23 +0300

    ✨ feature: add react routes

--- elewa/src/App.tsx
+++ elewa/src/App.tsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
 import Invest from './components/Invest';
 import About from './components/About';
+
 
 
 
 function App() {
   return (
     <div className="App">
-    {/* <Invest /> */}
-    <About />
+        <Routes>
+            <Route path="/" element={<About />} />
+            <Route path="/invest" element={<Invest />} />
+            // <Route path="/" element={<SocialImpact />} />
+        </Routes>
     </div>
   );
 }
