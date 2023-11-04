7a94c53dd1833ce2b3a9994e71106752b2b63947
Caren Nafula Wakuloba <canafula16@gmail.com>
2023-11-04 12:36:13 +0300

    ✨ feature:create content5 component

---
+++ elewa/src/components/Content5.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Content5(){
+    return (
+        <div>
+            <span>Some of our impact projects</span>
+            <span>Become part of our mission and join elewa.</span>
+        </div>
+    )
+}
+export default Content5;
