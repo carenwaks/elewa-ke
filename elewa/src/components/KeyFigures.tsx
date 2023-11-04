9fa28d3e9cb7692a00a4a98c7e154b3088b9272a
Caren Nafula Wakuloba <canafula16@gmail.com>
2023-11-04 14:48:35 +0300

    🚧 feature:start grid of key figures

---
+++ elewa/src/components/KeyFigures.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+// Defining the dataypes of the data used
+type FigureData = {
+      figure: string;
+      description: string;
+  };
+
+function KeyFigures(){
+    
+    return (
+        <div>
+            {/* Mapping the json data and display the data */}
+           {data.map((item: FigureData) => (
+          <div >
+            <p>{item.figure}</p>
+            <p>{item.description}</p>
+          </div>
+        ))} 
+            
+        
+    </div>
+    )
+};
+export default Hero;
