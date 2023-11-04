fcf5314763b017da6daf46bd18ed4fa2fed312d3
Caren Nafula Wakuloba <canafula16@gmail.com>
2023-11-04 10:29:14 +0300

    💄 feature:add content3 styling

--- elewa/src/components/Content3.tsx
+++ elewa/src/components/Content3.tsx
@@ -8,15 +8,15 @@ type ContentProps = {
 
 function Content3(props: ContentProps) {
     return (
-        <div className="overview-content1">
-            <div className="overview-img1">
+        <div className="overview-content3">
+            <div className="overview-img3">
                 <img
                 className="ov-img "
                 src={props.img}
                 alt="content image"
                 />
             </div>
-            <div className="overview-desc1">
+            <div className="overview-desc3">
                 <h4>{props.title}</h4>
                 <p >{props.text}</p>
             </div>
