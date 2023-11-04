294e3d7eb6cf74c7b5d1fe06b9da9e07ffcde7fe
Caren Nafula Wakuloba <canafula16@gmail.com>
2023-11-04 18:31:22 +0300

    💄 feature:update culture  styling

--- elewa/src/components/Culture.tsx
+++ elewa/src/components/Culture.tsx
@@ -2,31 +2,31 @@ import React from "react";
 
 function Culture(){
     return (
-        <div>
+        <div className="culture-sec">
             <h2>Our Culture & Values</h2>
             <p>Our culture, CODE-(T), defines us. It protects and guides us to success. It keeps our team and management accountable to itself, its objective, and our long-term mission.</p>
-            <div>
-            <div>
+            <div className="culture-details">
+            <div className="culture-slot">
             <img src="https://res.cloudinary.com/dyl3rncv3/image/upload/v1675690301/elewa-group-website/Icons/PNG/coorperative_kzlzrg.png"/>
             <h2>Cooperative</h2>
             <p>We go beyond a simple patch-up but develop lasting solutions through holistic design.</p>
             </div>
-            <div>
+            <div className="culture-slot">
                 <img src="https://res.cloudinary.com/dyl3rncv3/image/upload/v1675690301/elewa-group-website/Icons/PNG/ownership_yno4a2.png"/>
                 <h2>Ownership</h2>
                 <p>Impact as a direct, or indirect, result. All our respective organizations have underlying theories of change.</p>
             </div>
-            <div>
+            <div className="culture-slot">
                 <img src="https://static.thenounproject.com/png/1721983-200.png"/>
                 <h2>Detail</h2>
                 <p>Sharing is caring. We share what we learn. As proof, we've open-sourced all our internal projects.</p>
             </div>
-            <div>
+            <div className="culture-slot">
                 <img src="https://res.cloudinary.com/dyl3rncv3/image/upload/v1675690301/elewa-group-website/Icons/PNG/coorperative_kzlzrg.png" />
                 <h2>Empathy</h2>
                 <p>We are nothing without people, and people have different contexts and needs. Our organization acts as a safe haven and shield for those in need.</p>
             </div>
-            <div>
+            <div className="culture-slot">
                 <img src="https://res.cloudinary.com/dyl3rncv3/image/upload/v1675690300/elewa-group-website/Icons/PNG/Transparancy_fqal8q.png"/>
                 <h2>Transparency</h2>
                 <p>Transparency, an open mind, and an open heart hold our organization together. We commit to and promote full transparency to ourselves, our stakeholders, and our beneficiaries. We furthermore invest heavily in open-source and open-data projects, as true potential lies everywhere. Not just within.</p>
